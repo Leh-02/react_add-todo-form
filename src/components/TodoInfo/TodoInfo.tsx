381b9ba94db492e84551d69af772ca4b0b6a0128
Leh-02 <ivanlyash51@gmail.com>
2025-07-12 11:48:21 +0300

    some code with UserInfo problems

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,19 @@
-export const TodoInfo = () => {};
+import classNames from "classnames";
+import { Todo } from "../../types/Todo";
+import { getUserById } from "../../service/user";
+
+type Prop = {
+  todo: Todo;
+}
+
+export const TodoInfo:React.FC<Prop> = ({ todo}) => (
+  <article data-id="1" className={classNames("TodoInfo", {
+    "TodoInfo--completed": todo.completed,
+  })}>
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <a className="UserInfo" href="mailto:Sincere@april.biz">
+      {getUserById(todo.userId)?.name}
+    </a>
+  </article>
+);
